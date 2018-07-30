13f7bee968b5bc5697f0bfdbed9436a9a484319c
Cloyd Nino Catanaoan <antimone2001@hotmail.com>
2018-07-30 18:07:53 -0500

    Added line chart

---
+++ Living Room Temp and Humidity Chart/Objects/src/com/cloyd/livingroomtemphumidgraph/starter.java
@@ -0,0 +1,144 @@
+package com.cloyd.livingroomtemphumidgraph;
+
+
+import anywheresoftware.b4a.BA;
+import anywheresoftware.b4a.objects.ServiceHelper;
+import anywheresoftware.b4a.debug.*;
+
+public class starter extends  android.app.Service{
+	public static class starter_BR extends android.content.BroadcastReceiver {
+
+		@Override
+		public void onReceive(android.content.Context context, android.content.Intent intent) {
+            BA.LogInfo("** Receiver (starter) OnReceive **");
+			android.content.Intent in = new android.content.Intent(context, starter.class);
+			if (intent != null)
+				in.putExtra("b4a_internal_intent", intent);
+            ServiceHelper.StarterHelper.startServiceFromReceiver (context, in, true, BA.class);
+		}
+
+	}
+    static starter mostCurrent;
+	public static BA processBA;
+    private ServiceHelper _service;
+    public static Class<?> getObject() {
+		return starter.class;
+	}
+	@Override
+	public void onCreate() {
+        super.onCreate();
+        mostCurrent = this;
+        if (processBA == null) {
+		    processBA = new BA(this, null, null, "com.cloyd.livingroomtemphumidgraph", "com.cloyd.livingroomtemphumidgraph.starter");
+            if (BA.isShellModeRuntimeCheck(processBA)) {
+                processBA.raiseEvent2(null, true, "SHELL", false);
+		    }
+            try {
+                Class.forName(BA.applicationContext.getPackageName() + ".main").getMethod("initializeProcessGlobals").invoke(null, null);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+            processBA.loadHtSubs(this.getClass());
+            ServiceHelper.init();
+        }
+        _service = new ServiceHelper(this);
+        processBA.service = this;
+        
+        if (BA.isShellModeRuntimeCheck(processBA)) {
+			processBA.raiseEvent2(null, true, "CREATE", true, "com.cloyd.livingroomtemphumidgraph.starter", processBA, _service, anywheresoftware.b4a.keywords.Common.Density);
+		}
+        if (!true && ServiceHelper.StarterHelper.startFromServiceCreate(processBA, false) == false) {
+				
+		}
+		else {
+            processBA.setActivityPaused(false);
+            BA.LogInfo("*** Service (starter) Create ***");
+            processBA.raiseEvent(null, "service_create");
+        }
+        processBA.runHook("oncreate", this, null);
+        if (true) {
+			ServiceHelper.StarterHelper.runWaitForLayouts();
+		}
+    }
+		@Override
+	public void onStart(android.content.Intent intent, int startId) {
+		onStartCommand(intent, 0, 0);
+    }
+    @Override
+    public int onStartCommand(final android.content.Intent intent, int flags, int startId) {
+    	if (ServiceHelper.StarterHelper.onStartCommand(processBA, new Runnable() {
+            public void run() {
+                handleStart(intent);
+            }}))
+			;
+		else {
+			ServiceHelper.StarterHelper.addWaitForLayout (new Runnable() {
+				public void run() {
+                    processBA.setActivityPaused(false);
+                    BA.LogInfo("** Service (starter) Create **");
+                    processBA.raiseEvent(null, "service_create");
+					handleStart(intent);
+                    ServiceHelper.StarterHelper.removeWaitForLayout();
+				}
+			});
+		}
+        processBA.runHook("onstartcommand", this, new Object[] {intent, flags, startId});
+		return android.app.Service.START_NOT_STICKY;
+    }
+    public void onTaskRemoved(android.content.Intent rootIntent) {
+        super.onTaskRemoved(rootIntent);
+        if (true)
+            processBA.raiseEvent(null, "service_taskremoved");
+            
+    }
+    private void handleStart(android.content.Intent intent) {
+    	BA.LogInfo("** Service (starter) Start **");
+    	java.lang.reflect.Method startEvent = processBA.htSubs.get("service_start");
+    	if (startEvent != null) {
+    		if (startEvent.getParameterTypes().length > 0) {
+    			anywheresoftware.b4a.objects.IntentWrapper iw = ServiceHelper.StarterHelper.handleStartIntent(intent, _service, processBA);
+    			processBA.raiseEvent(null, "service_start", iw);
+    		}
+    		else {
+    			processBA.raiseEvent(null, "service_start");
+    		}
+    	}
+    }
+	
+	@Override
+	public void onDestroy() {
+        super.onDestroy();
+        BA.LogInfo("** Service (starter) Destroy **");
+		processBA.raiseEvent(null, "service_destroy");
+        processBA.service = null;
+		mostCurrent = null;
+		processBA.setActivityPaused(true);
+        processBA.runHook("ondestroy", this, null);
+	}
+
+@Override
+	public android.os.IBinder onBind(android.content.Intent intent) {
+		return null;
+	}public anywheresoftware.b4a.keywords.Common __c = null;
+public com.cloyd.livingroomtemphumidgraph.main _main = null;
+public static String  _process_globals() throws Exception{
+ //BA.debugLineNum = 6;BA.debugLine="Sub Process_Globals";
+ //BA.debugLineNum = 10;BA.debugLine="End Sub";
+return "";
+}
+public static String  _service_create() throws Exception{
+ //BA.debugLineNum = 12;BA.debugLine="Sub Service_Create";
+ //BA.debugLineNum = 16;BA.debugLine="End Sub";
+return "";
+}
+public static String  _service_destroy() throws Exception{
+ //BA.debugLineNum = 22;BA.debugLine="Sub Service_Destroy";
+ //BA.debugLineNum = 24;BA.debugLine="End Sub";
+return "";
+}
+public static String  _service_start(anywheresoftware.b4a.objects.IntentWrapper _startingintent) throws Exception{
+ //BA.debugLineNum = 18;BA.debugLine="Sub Service_Start (StartingIntent As Intent)";
+ //BA.debugLineNum = 20;BA.debugLine="End Sub";
+return "";
+}
+}
